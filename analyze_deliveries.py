2c956f6aa4c6790161341db041392141f2e4b704
Cristóbal Vio <cristobal.vio@gmail.com>
2017-05-27 10:26:43 +0200

    better formatting of the scripts

--- analyze_deliveries.py
+++ analyze_deliveries.py
@@ -1,6 +1,6 @@
 # -*- coding: latin-1 -*-
 import json
-import datetime 
+import datetime
 from pprint import pprint
 import argparse
 
@@ -16,23 +16,24 @@ if args['daterange'] is not None:
     fnldates = args['daterange'].split("-")
     if len(fnldates) != 2:
         raise Exception('Use YYmmdd-YYmmdd format for specifying date range')
-    fs_datetime = datetime.datetime.strptime(fnldates[0],'%Y%m%d')
+    fs_datetime = datetime.datetime.strptime(fnldates[0], '%Y%m%d')
     fs_day = fs_datetime.date()
-    ls_datetime = datetime.datetime.strptime(fnldates[1],'%Y%m%d')
+    ls_datetime = datetime.datetime.strptime(fnldates[1], '%Y%m%d')
     ls_day = ls_datetime.date()
-    ls_datetime = ls_datetime+d_delta 
+    ls_datetime = ls_datetime + d_delta
     if fs_day > ls_day:
-        raise Exception('First day in date range cannot be later than the last day')
+        raise Exception(
+            'First day in date range cannot be later than the last day')
 
 f = open(args['jsonfile'], 'r')
 deliveries = json.load(f)
 
 # arreglar esto para que sea más general
-max_date = datetime.datetime.today()-datetime.timedelta(days=7)
-min_date = datetime.datetime.today()-datetime.timedelta(days=7)
+max_date = datetime.datetime.today() - datetime.timedelta(days=7)
+min_date = datetime.datetime.today() - datetime.timedelta(days=7)
 
 for delivery in deliveries:
-    deldate = datetime.datetime.strptime(delivery['date'],'%Y-%m-%d %H:%M:%S')
+    deldate = datetime.datetime.strptime(delivery['date'], '%Y-%m-%d %H:%M:%S')
     delivery['date_obj'] = deldate
     if deldate > max_date:
         max_date = deldate
@@ -43,24 +44,24 @@ if args['daterange'] is None:
 
     fs_datetime = datetime.datetime(min_date.year, min_date.month, min_date.day)
     fs_day = fs_datetime.date()
-    ls_datetime = datetime.datetime(max_date.year, max_date.month, max_date.day)#+d_delta
+    ls_datetime = datetime.datetime(max_date.year, max_date.month, max_date.day) # +d_delta
     ls_day = ls_datetime.date()
-    ls_datetime = ls_datetime+d_delta 
+    ls_datetime = ls_datetime + d_delta
 
     statstitle = "=== All-time statistics ==="
 else:
     statstitle = "=== Specified period statistics ==="
 
-deliveries_n = [delivery for delivery in deliveries if delivery['date_obj']>fs_datetime and delivery['date_obj']<ls_datetime]
+deliveries_n = [delivery for delivery in deliveries if delivery['date_obj'] > fs_datetime and delivery['date_obj'] < ls_datetime]
 
-deltimes_n = [int(delivery['deltime_s']) for delivery in deliveries_n] 
+deltimes_n = [int(delivery['deltime_s']) for delivery in deliveries_n]
 tip_money_n = [float(delivery['tip_cc']) for delivery in deliveries_n]
 
 ndel_r = len(deliveries_n)
 
 if ndel_r != 0:
-    mean_delivery_time_s_n = float(sum(deltimes_n))/ndel_r
-    mean_time_string = "\t\t{:.1f} s".format(mean_delivery_time_s_n) 
+    mean_delivery_time_s_n = float(sum(deltimes_n)) / ndel_r
+    mean_time_string = "\t\t{:.1f} s".format(mean_delivery_time_s_n)
 else:
     mean_time_string = "\t\t-"
 
@@ -68,16 +69,16 @@ total_tips_by_cc_n = sum(tip_money_n)
 
 working_period_r = ls_day - fs_day
 
-date_objs_n = [fs_day + d_delta*i for i in range(working_period_r.days+1)]
+date_objs_n = [fs_day + d_delta * i for i in range(working_period_r.days + 1)]
 #pprint(date_objs)
-dates_n = [(date.year,date.month,date.day) for date in date_objs_n]
-desglose = {date_tuple:[] for date_tuple in dates_n}
-fdelpd = {date_tuple:datetime.time(23,59,59) for date_tuple in dates_n}
-ldelpd = {date_tuple:datetime.time(0,0,0) for date_tuple in dates_n}
+dates_n = [(date.year, date.month, date.day) for date in date_objs_n]
+desglose = {date_tuple: [] for date_tuple in dates_n}
+fdelpd = {date_tuple: datetime.time(23, 59, 59) for date_tuple in dates_n}
+ldelpd = {date_tuple: datetime.time(0, 0, 0) for date_tuple in dates_n}
 
 for delivery in deliveries_n:
-    ddate=delivery['date_obj']
-    dtuple=(ddate.year,ddate.month,ddate.day)
+    ddate = delivery['date_obj']
+    dtuple = (ddate.year, ddate.month, ddate.day)
     desglose[dtuple].append(delivery)
     if delivery['date_obj'].time() < fdelpd[dtuple]:
         fdelpd[dtuple] = delivery['date_obj'].time()
@@ -89,7 +90,7 @@ print(statstitle)
 print("")
 print("min date:\t%s" % fs_day)
 print("max date:\t%s" % ls_day)
-print("working period:\t%d days" % (working_period_r.days+1))
+print("working period:\t%d days" % (working_period_r.days + 1))
 print("deliveries:\t%s" % len(deliveries_n))
 print("Mean delivery time:")
 print(mean_time_string)
@@ -97,20 +98,20 @@ print("Tips with creditcard:")
 print("\t\t%.2f euro" % total_tips_by_cc_n)
 print("")
 
-dsotw = {1:'mon',2:'tue',3:'wed',4:'thu',5:'fri',6:'sat',7:'sun'}
+dsotw = {1: 'mon', 2: 'tue', 3: 'wed', 4: 'thu', 5: 'fri', 6: 'sat', 7: 'sun'}
 
 if args['verbose']:
     print("    date\tdels\tmean_t\ttip_cc\ttofdel\t\ttoldel")
 
     for date in dates_n:
-        ndel=len(desglose[date])
-        dow=datetime.date(date[0],date[1],date[2]).isoweekday()
-        tipmoney=sum([float(delivery['tip_cc']) for delivery in desglose[date]])
+        ndel = len(desglose[date])
+        dow = datetime.date(date[0], date[1],date[2]).isoweekday()
+        tipmoney = sum([float(delivery['tip_cc']) for delivery in desglose[date]])
         if ndel:
-            meandtime=float(sum([delivery['deltime_s'] for delivery in desglose[date]]))/ndel
+            meandtime = float(sum([delivery['deltime_s'] for delivery in desglose[date]])) / ndel
             print("%s %d-%02d-%02d\t%4d\t%.2f\t%.2f\t%s\t%s" % (dsotw[dow],date[0],date[1],date[2],ndel,meandtime,tipmoney,fdelpd[date],ldelpd[date]))
         else:
             #ndel='-'
-            print("%s %d-%02d-%02d\t  -\t-\t-\t-\t-" % (dsotw[dow],date[0],date[1],date[2]))
+            print("%s %d-%02d-%02d\t  -\t-\t-\t-\t\t-" % (dsotw[dow], date[0], date[1], date[2]))
     
     print("")
--- get_deliveroo_mails.py
+++ get_deliveroo_mails.py
@@ -71,8 +71,6 @@ def main():
     #search_label = 'Label_15' # entrega-deliveroo
     citycode = {'Berlin':8}
 
-    #results = service.users().labels().list(userId='me').execute()
-    #results = service.users().messages().list(userId='me',labelIds=search_label).execute()
     results = service.users().threads().list(userId='me',labelIds=search_label).execute()
     hilos = results.get('threads', [])
 
@@ -81,65 +79,59 @@ def main():
         results = service.users().threads().list(userId='me',labelIds=search_label, pageToken=page_token).execute()
         hilos.extend(results.get('threads', []))
 
-    #print(type(threads))
-    #print(type(threads[0]))
-    #print(threads[0].keys())
-    #print(type(threads[0]['id']))
-    #print(type(threads[0]['threadId']))
-
     if os.path.isfile(flags['jsonfile']):
-      f = open(flags['jsonfile'], 'r')
-      deliveries = json.load(f)
-      f.close()
+        f = open(flags['jsonfile'], 'r')
+        deliveries = json.load(f)
+        f.close()
     else:
-      deliveries = []
+        deliveries = []
 
-    nres=0
-    nres_new=0
+    nres = 0
+    nres_new = 0
 
-    transl = {'Geliefert um':'date', 'Lieferzeit':'deltime', 'Restaurant':'restaurant', 'Restaurantadresse':'r_address', 'Restaurant Telefon':'r_tel', 'Trinkgeld (Kreditkarte)':'tip_cc'}
+    transl = {'Geliefert um': 'date', 'Lieferzeit': 'deltime', 'Restaurant': 'restaurant', 'Restaurantadresse': 'r_address', 'Restaurant Telefon': 'r_tel', 'Trinkgeld (Kreditkarte)': 'tip_cc'}
 
     if not hilos:
         print('No threads found.')
     else:
-      print('Messages:')
-      thread_ids = [delivery['thread_id'] for delivery in deliveries]
-      for hilo in hilos:
-        #print(message['id'],message['threadId'])
-        if not hilo['id'] in thread_ids:
-          thread = service.users().threads().get(userId='me', id=hilo['id'], format='full').execute()
-          mail_string = str(base64.urlsafe_b64decode(thread['messages'][0]['payload']['body']['data'].encode('UTF8')))
-          ms_lines = mail_string.split('\\r\\n')
-          delivery_info = {}
-          delivery_info['thread_id'] = hilo['id']
-          for ms_line in ms_lines:
-              match_field = re.search('([^:]*): (.*)',ms_line)
-              match_nr = re.search('Bestellung #([0-9]+) geliefert',ms_line)
-              if match_field:
-                  delivery_info[transl[match_field.group(1)]]=match_field.group(2)
-              elif match_nr:
-                  delivery_info['daily_id']=match_nr.group(1)
-          delivery_info['deltime_s'] = minsec2sec(delivery_info['deltime'])
-          deldate=datetime.datetime.strptime(delivery_info['date'],'%Y-%m-%d %H:%M:%S')
-          delivery_info['id']= int(delivery_info['daily_id'])+10000*deldate.hour+1000000*deldate.day+100000000*deldate.month+10000000000*(deldate.year%100)+1000000000000*citycode['Berlin']
-          deliveries.append(delivery_info)
-          nres_new=nres_new+1
-        nres=nres+1
+        print('Messages:')
+        thread_ids = [delivery['thread_id'] for delivery in deliveries]
+        for hilo in hilos:
+            if not hilo['id'] in thread_ids:
+                thread = service.users().threads().get(userId='me', id=hilo['id'], format='full').execute()
+                mail_string = str(base64.urlsafe_b64decode(thread['messages'][0]['payload']['body']['data'].encode('UTF8')))
+                ms_lines = mail_string.split('\\r\\n')
+                delivery_info = {}
+                delivery_info['thread_id'] = hilo['id']
+                for ms_line in ms_lines:
+                    match_field = re.search('([^:]*): (.*)',ms_line)
+                    match_nr = re.search('Bestellung #([0-9]+) geliefert',ms_line)
+                    if match_field:
+                        delivery_info[transl[match_field.group(1)]]=match_field.group(2)
+                    elif match_nr:
+                        delivery_info['daily_id']=match_nr.group(1)
+                delivery_info['deltime_s'] = minsec2sec(delivery_info['deltime'])
+                deldate=datetime.datetime.strptime(delivery_info['date'],'%Y-%m-%d %H:%M:%S')
+                delivery_info['id']= int(delivery_info['daily_id'])+10000*deldate.hour+1000000*deldate.day+100000000*deldate.month+10000000000*(deldate.year%100)+1000000000000*citycode['Berlin']
+                deliveries.append(delivery_info)
+                nres_new=nres_new+1
+            nres=nres+1
     print(nres)
     print("%d of them are new." % nres_new)
 
     f = open(flags['jsonfile'], 'w')
-    json.dump(deliveries,f)
+    json.dump(deliveries, f)
     f.close()
+
 
 def minsec2sec(s):
     """ Parsea un string con minutos y segundos y devuelve el numero
         de segundos correspondiente
         s: es un string describiendo el tiempo con formato 'MMm SSs'
     """
-    match = re.search('([0-9]+)m ([0-9]+)s',s)
+    match = re.search('([0-9]+)m ([0-9]+)s', s)
     if match:
-        return int(match.group(1))*60 + int(match.group(2))
+        return int(match.group(1)) * 60 + int(match.group(2))
     else:
         return -1
 
